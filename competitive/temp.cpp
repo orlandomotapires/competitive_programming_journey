6a717bf2fe56c2c131562c87e546a9f7607a9b09
orlandomotapires <orlandomota2002@gmail.com>
2023-07-26 18:34:15 -0300

    my solutions

---
+++ competitive/temp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+
+typedef long long ll;
+
+int main(){
+    
+    
+    return 0;
+}
