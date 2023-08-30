6ea8f840edc9a82d0b7f91bd4580d4f802686675
Fernando Schettini <fernando.schettini@Outlook.com>
2023-08-30 16:40:39 -0300

    3144

---
+++ competitive/uri-cpp/1923.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+
+typedef long long ll;
+
+map<string, int> nomes;
+
+int main(){
+    int n, m; cin >> n >> m;
+
+    string n1, n2;
+    for(int i = 0; i<n; i++){
+        cin >> n1 >> n2;
+        nomes[n1] = 0;
+        nomes[n2] = 0;
+    }
+
+    
+
+    
+    return 0;
+}
