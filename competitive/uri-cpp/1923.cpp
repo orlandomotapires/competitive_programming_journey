45f388bc795b8d02ba3905a028b5009f1b5e1d9b
orlandomotapires <orlandomota2002@gmail.com>
2023-08-31 20:01:24 -0300

    1923.cpp

--- competitive/uri-cpp/1923.cpp
+++ competitive/uri-cpp/1923.cpp
@@ -9,20 +9,68 @@ using namespace std;
 
 typedef long long ll;
 
+const int MAX = 1e4+10;
+//   nome   num
 map<string, int> nomes;
 
+vector<vector<int>> g(MAX);
+set<int> res;
+
+int vis[MAX];
+int m;
+
+void bfs(int v){
+
+    list<int> queue;
+    vis[v] = 1;
+
+    queue.push_back(v);
+    int grau_atu = 1;
+ 
+    while (!queue.empty()) {
+ 
+        v = queue.front();
+        queue.pop_front();
+
+        for (auto u : g[v]) {
+            if (!vis[u]) {
+                vis[u] = 1;
+                queue.push_back(u);  
+                if(queue.size() == g[u].size()) grau_atu++;
+            }        
+            if(grau_atu <= m) res.insert(u);
+        }     
+    }
+}
+
 int main(){
-    int n, m; cin >> n >> m;
+    int n; cin >> n >> m;
 
     string n1, n2;
-    for(int i = 0; i<n; i++){
+    memset(vis, 0, sizeof(vis));
+
+    for(int i = 0, j=0; i<n; i++){
         cin >> n1 >> n2;
-        nomes[n1] = 0;
-        nomes[n2] = 0;
+
+        if(nomes.find(n1) == nomes.end()){
+            nomes[n1] = j; j++;
+        }
+        
+        if(nomes.find(n2) == nomes.end()){
+            nomes[n2] = j; j++;
+        }
+            
+        g[nomes[n1]].push_back(nomes[n2]);
+        g[nomes[n2]].push_back(nomes[n1]);
     }
 
+    bfs(0);
+    res.erase(0);
     
+    cout << res.size() << endl;
+    for(auto j: nomes){
+        if(res.count(j.second)) cout << j.first << endl;
+    }
 
-    
     return 0;
 }
