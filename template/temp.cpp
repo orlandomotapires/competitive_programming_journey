0a0873d6656873cbed988c3e29fa0a99680b81e2
orlandomotapires <orlandomota2002@gmail.com>
2024-03-06 21:01:55 -0300

    updated temp.cpp

--- template/temp.cpp
+++ template/temp.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+ 
+#define _ ios_base::sync_with_stdio(false);cin.tie(nullptr);
 #define endl '\n'
 #define f first
 #define s second
+ 
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
 
 typedef long long ll;
 // const int MAX = ;
 
 int main(){
-    
     
     return 0;
 }
