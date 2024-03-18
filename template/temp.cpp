d0f9265a696bc6df4193c421bb6ece3a2b21a3a4
orlandomotapires <orlandomota2002@gmail.com>
2024-03-17 21:09:40 -0300

    att template

--- template/temp.cpp
+++ template/temp.cpp
@@ -1,18 +1,30 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
+// Macros ---
 #define _ ios_base::sync_with_stdio(false);cin.tie(nullptr);
+#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define fr(x, y) for(int i = x; i < y; i++)
+#define pb(x) push_back(x);
 #define endl '\n'
 #define f first
 #define s second
- 
-#define dbg(v) cout << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << endl;
+#define mod 1000000007
 
+// TypeDefs ---
 typedef long long ll;
-// const int MAX = ;
+
+// Consts ---
+const ll MAX = 1e7;
+
+// Variables ---
+ll t = 1;
 
 int main(){
-    
+
+    while(t--){
+
+    }
     return 0;
 }
